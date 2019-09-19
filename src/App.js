d3c5c673dad31574c35af48fdda821bcdb6e9a43
lowell1 <lowelljacobs@mail.com>
2019-09-18 20:26:34 -0700

    finished mvp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Form from "./Form";
+import Member from "./Member";
 
 function App() {
+  const [members, setMembers] = useState([]);
+
+  const addNewMember = newMember => {
+    setMembers([...members, newMember]);
+    console.log("members = ", members);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Form addNewMember={addNewMember}/>
+      {members.map((mem, idx) => {
+        return <Member key={idx} name={mem.name} role={mem.role} email={mem.email}/>;
+      })}
     </div>
   );
 }
